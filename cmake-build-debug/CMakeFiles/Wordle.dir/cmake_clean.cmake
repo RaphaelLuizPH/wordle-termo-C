09aeb64d76edae9c7d76a5334d15e4e993ace049
Lucas <lucasataide.martins@a.ucb.br>
2024-11-24 13:00:53 -0300

    commit pela primeira vez com Clion

---
+++ cmake-build-debug/CMakeFiles/Wordle.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wordle.dir/lib/backupPalavras.c.obj"
+  "CMakeFiles/Wordle.dir/lib/backupPalavras.c.obj.d"
+  "CMakeFiles/Wordle.dir/lib/cores.c.obj"
+  "CMakeFiles/Wordle.dir/lib/cores.c.obj.d"
+  "CMakeFiles/Wordle.dir/lib/creditos.c.obj"
+  "CMakeFiles/Wordle.dir/lib/creditos.c.obj.d"
+  "CMakeFiles/Wordle.dir/lib/menu.c.obj"
+  "CMakeFiles/Wordle.dir/lib/menu.c.obj.d"
+  "CMakeFiles/Wordle.dir/lib/processarTentativa.c.obj"
+  "CMakeFiles/Wordle.dir/lib/processarTentativa.c.obj.d"
+  "CMakeFiles/Wordle.dir/main.c.obj"
+  "CMakeFiles/Wordle.dir/main.c.obj.d"
+  "Wordle.exe"
+  "Wordle.exe.manifest"
+  "Wordle.pdb"
+  "libWordle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Wordle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
